c47b4e7a8fa20cc14a0e44c1a25f57979fc25c48
simplefunction <kei97111@gmail.com>
2026-01-07 20:52:30 +0900

    modify status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,31 @@
 import {Stack} from 'expo-router';
 import {LocaleProvider, useLocale} from '../src/i18n/LocaleProvider';
 import {SafeAreaProvider} from "react-native-safe-area-context";
+import {StatusBar} from "react-native";
 
 function RootStack() {
     const {locale} = useLocale();
 
     return (
-        <Stack key={locale}>
-            <Stack.Screen name="index" options={{headerShown: false}}/>
-            <Stack.Screen name="language" options={{headerShown: false}}/>
-            <Stack.Screen name="onboarding" options={{headerShown: false}}/>
-            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-            <Stack.Screen
-                name="modal"
-                options={{
-                    presentation: 'modal',
-                    headerShown: false, // 모달 자체 헤더는 안 보이게(내부 레이아웃에서 관리)
-                    animation: 'slide_from_bottom', // iOS 스타일 애니메이션
-                    gestureEnabled: true, // 스와이프 제스처
-                }}
-            />
+        <>
+            <StatusBar backgroundColor="#FFA07A" barStyle="dark-content"/>
+            <Stack key={locale}>
+                <Stack.Screen name="index" options={{headerShown: false}}/>
+                <Stack.Screen name="language" options={{headerShown: false}}/>
+                <Stack.Screen name="onboarding" options={{headerShown: false}}/>
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+                <Stack.Screen
+                    name="modal"
+                    options={{
+                        presentation: 'modal',
+                        headerShown: false, // 모달 자체 헤더는 안 보이게(내부 레이아웃에서 관리)
+                        animation: 'slide_from_bottom', // iOS 스타일 애니메이션
+                        gestureEnabled: true, // 스와이프 제스처
+                    }}
+                />
 
-        </Stack>
+            </Stack>
+        </>
     );
 }
 
